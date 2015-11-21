8dbc559d5d7cfb5a877d8072a77c02bcbe96d45a
Richard Lincoln <r.w.lincoln@gmail.com>
2015-11-21 13:27:16 +0100

    Adding OSM tile layer for convenience.

--- lib/layer/tile.dart
+++ lib/layer/tile.dart
@@ -3,6 +3,12 @@ part of leaflet;
 const osmTileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const mapQuestUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
 
+final osmLayer = new TileLayer(
+    url: osmTileUrl,
+    attribution:
+        'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18);
+
 /*
 class TileLayerOptions {
   /// Minimum zoom number. Default: 0
